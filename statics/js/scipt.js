e0b6e7e5d45474578cc55e27ad7bc8f8908540f6
educoelho03 <eduardopaivacoelho@oulook.com>
2023-04-04 22:48:00 -0300

    main

---
+++ statics/js/scipt.js
@@ -0,0 +1,19 @@
+document.querySelector("#qtde").addEventListener("change", calcularOrcamento);
+document.querySelector("#js").addEventListener("change", calcularOrcamento);
+document.querySelector("#layout_s").addEventListener("change", calcularOrcamento);
+document.querySelector("#layout_n").addEventListener("change", calcularOrcamento);
+document.querySelector("#prazo").addEventListener("change", calcularOrcamento);
+
+
+function calcularOrcamento(){
+
+   let preco = qtde.value * 100;
+   if(js.checked) preco *= 1.1
+   if(layout_s.checked) preco += 500
+
+   let taxaDeUgencia = preco * (1.1 + prazo.value * 0.1)
+   preco += taxaDeUgencia
+
+   label_prazo.innerHTML = `Prazo (${prazo.value} semanas)`
+   output.innerHTML = "R$ " + preco.toFixed(2)
+}
