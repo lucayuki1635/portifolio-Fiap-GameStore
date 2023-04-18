1f79ba7df6012916e887748991fcb3b61ecc1cd7
educoelho03 <eduardopaivacoelho@outlook.com>
2023-04-18 20:32:41 -0300

    hamburguer

--- statics/js/menu.js
+++ statics/js/menu.js
@@ -1,6 +1,25 @@
+let mostrando = false;
+
 document.querySelector(".hamburguer").addEventListener("click", mostrar_menu)
+document.addEventListener("scroll",esconder_menu)
 
 function mostrar_menu(){
     document.querySelector(".menu").classList.toggle("show-menu")
+    if(mostrando==false){
+        mostrando= true
+
+    }else{
+        mostrando = false
+    }
     console.log("Mostrar menu")
 }
+
+
+function esconder_menu() {
+  if(mostrando==true){
+    document.querySelector(".menu").classList.toggle("show-menu")
+    mostrando = false
+  }
+}
+
+
